947230fab8f6f02acd175cda33a97e2edd39b941
iHelin <ihelin@outlook.com>
2021-04-17 21:23:02 +0800

    1

--- pages/about/index.js
+++ pages/about/index.js
@@ -2,39 +2,42 @@ import request from "../../utils/request";
 
 Page({
     data: {
-        articleContent: ""
+        articleContent: "",
     },
     onLoad() {
         this.getArticle();
     },
     async getArticle() {
-        const result = await request('/wechat/article/info', {
-            type: 'about'
+        const result = await request("/wechat/article/info", {
+            type: "about",
         });
         if (result.code !== 0) {
             wx.showModal({
-                title: '提示',
+                title: "提示",
                 content: result.msg,
-                showCancel: false
+                showCancel: false,
             });
         } else {
             let article = result.data;
-            const articleContent = article.content.replace(/<img/gi, '<img style="max-width:100%;height:auto"');
+            const articleContent = article.content.replace(
+                /<img/gi,
+                '<img style="max-width:100%;height:auto"'
+            );
             this.setData({
-                articleContent
+                articleContent,
             });
         }
     },
     onShareAppMessage() {
         return {
-            title: '糖衣天使',
-            path: '/pages/index/index?inviterId=' + wx.getStorageSync('userId')
-        }
+            title: "糖衣天使",
+            path: "/pages/index/index?inviterId=" + wx.getStorageSync("userId"),
+        };
     },
     onShareTimeline() {
         return {
-            title: '糖衣天使',
-            query: 'inviterId=' + wx.getStorageSync('userId')
-        }
-    }
-})
+            title: "糖衣天使",
+            query: "inviterId=" + wx.getStorageSync("userId"),
+        };
+    },
+});

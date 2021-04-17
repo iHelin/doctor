a1a37eb5674b19842eed2add4e23609970272479
iHelin <ihelin@outlook.com>
2021-04-17 09:57:03 +0800

    1

--- pages/me/index.js
+++ pages/me/index.js
@@ -20,37 +20,35 @@ Page({
             });
         }
     },
-    login() {
-        wx.getUserInfo({
-            success: (result) => {
-                let userInfo = result.userInfo;
-                wx.login({
-                    success: async (res) => {
-                        const result = await request(
-                            "/wechat/login",
-                            {
-                                code: res.code,
-                                nickname: userInfo.nickName,
-                                avatarUrl: userInfo.avatarUrl,
-                            },
-                            "POST"
-                        );
-                        if (result.code === 0) {
-                            this.setData({
-                                hasLogin: true,
-                            });
-                            wx.setStorageSync("token", result.data.token);
-                            wx.setStorageSync("userId", result.data.id);
-                            this.checkBinding(result.data);
-                        }
-                    },
-                });
-            },
-        });
+    login(e) {
+        if (e.detail.errMsg === "getUserInfo:ok") {
+            let userInfo = e.detail.userInfo;
+            wx.login({
+                success: async (res) => {
+                    const result = await request(
+                        "/wechat/login",
+                        {
+                            code: res.code,
+                            nickname: userInfo.nickName,
+                            avatarUrl: userInfo.avatarUrl,
+                        },
+                        "POST"
+                    );
+                    if (result.code === 0) {
+                        this.setData({
+                            hasLogin: true,
+                        });
+                        wx.setStorageSync("token", result.data.token);
+                        this.getUserInfo(result.data);
+                    }
+                },
+            });
+        }
     },
     async getUserInfo() {
         const result = await request("/wechat/me", {}, "get");
         if (result.code === 0) {
+            wx.setStorageSync("userId", result.data.id);
             this.setData({
                 id: result.data.id,
                 nickname: result.data.nickname,
@@ -64,8 +62,6 @@ Page({
     },
     checkBinding(data) {
         if (data.binding) {
-            const userId = data.id;
-            wx.setStorageSync("userId", userId);
             wx.setStorageSync("username", data.username);
             wx.setStorageSync("idCard", data.idCard);
             wx.setStorageSync("mobile", data.mobile);
